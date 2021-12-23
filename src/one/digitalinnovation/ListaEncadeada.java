a29172d6034ed49e26b437f69e7f0c4e80bbe2cd
obarreto <ozbarreto@outlook.com>
2021-12-22 22:45:22 -0300

    Implementação da classe ListaEncadeada(get())

--- src/one/digitalinnovation/ListaEncadeada.java
+++ src/one/digitalinnovation/ListaEncadeada.java
@@ -23,6 +23,10 @@ public class ListaEncadeada<T> {
 		noAuxiliar.setProximoNo(novoNo);
 	}
 	
+	public T get(int index) {
+		return getNo(index).getConteudo();
+	}
+	
 	private No<T> getNo(int index){
 		
 		validaIndice(index);
