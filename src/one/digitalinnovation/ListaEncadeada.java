c9ef1b3791e7fb0faeb4f4b80fbb3d1df9a6fb45
obarreto <ozbarreto@outlook.com>
2021-12-22 22:01:05 -0300

    Implementação da classe ListaEncadeada (isEmpty e size())

---
+++ src/one/digitalinnovation/ListaEncadeada.java
@@ -0,0 +1,32 @@
+package one.digitalinnovation;
+
+public class ListaEncadeada<T> {
+
+	No<T> referenciaEntrada;
+	
+	public ListaEncadeada(){
+		this.referenciaEntrada = null;
+	}
+	
+	public int size() {
+		int tamanhoLista = 0;
+		No<T> referenciaAux = referenciaEntrada;
+		while(true) {
+			if(referenciaAux != null) {
+				tamanhoLista++;
+				if(referenciaAux.getProximoNo() != null) {
+					referenciaAux = referenciaAux.getProximoNo();
+				}else {
+					break;
+				}
+			}else { 
+				break;
+			}
+		}
+		return tamanhoLista;
+	}
+	
+	public boolean isEmpty() {
+		return referenciaEntrada == null ? true : false;
+	}
+}
---
+++ src/one/digitalinnovation/No.java
@@ -0,0 +1,47 @@
+package one.digitalinnovation;
+
+public class No<T> {
+
+	private T conteudo;
+	private No proximoNo;
+	
+	
+	public No() {
+		this.proximoNo = null;
+	}
+	
+	public No(T conteudo, No proximoNo) {
+		this.conteudo = conteudo;
+		this.proximoNo = proximoNo;
+	}
+
+	public T getConteudo() {
+		return conteudo;
+	}
+	public void setConteudo(T conteudo) {
+		this.conteudo = conteudo;
+	}
+	public No getProximoNo() {
+		return proximoNo;
+	}
+	public void setProximo(No proximoNo) {
+		this.proximoNo = proximoNo;
+	}
+
+	@Override
+	public String toString() {
+		return "No [conteudo=" + conteudo + ", proximoNo=" + proximoNo + "]";
+	}
+	
+	public String toStringEncadeado() {
+		String str = "No{" +
+				"conteudo=" + conteudo + '}';
+		
+		if(proximoNo != null) {
+			str += "->" + proximoNo.toString();
+		}else {
+			str += "->null";
+		}
+		return str;
+	}
+}
