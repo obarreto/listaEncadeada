e186426edc1d9bcb69da69c6566df592fe6aaed1
obarreto <ozbarreto@outlook.com>
2021-12-22 22:40:55 -0300

    Implementação da classe ListaEncadeada(getNo())

--- src/one/digitalinnovation/ListaEncadeada.java
+++ src/one/digitalinnovation/ListaEncadeada.java
@@ -23,6 +23,19 @@ public class ListaEncadeada<T> {
 		noAuxiliar.setProximoNo(novoNo);
 	}
 	
+	private No<T> getNo(int index){
+		
+		validaIndice(index);
+		No<T> noAuxiliar = referenciaEntrada;
+		No<T> noRetorno = null;
+		
+		for(int i = 0; i < this.size()-1; i++) {
+			noRetorno = noAuxiliar;
+			noAuxiliar = noAuxiliar.getProximoNo();
+		}
+		return noRetorno;
+	}
+	
 	public int size() {
 		int tamanhoLista = 0;
 		No<T> referenciaAux = referenciaEntrada;
@@ -39,6 +52,15 @@ public class ListaEncadeada<T> {
 			}
 		}
 		return tamanhoLista;
+	}
+	
+	private void validaIndice(int index) {
+		if(index >= size()) {
+			int ultimoIndice = size()-1;
+			throw new IndexOutOfBoundsException("Não existe conteudo no indice " + index 
+					+ "desta lista. Esta lista só vai até o indice" 
+					+ ultimoIndice + ".");
+		}
 	}
 	
 	public boolean isEmpty() {
